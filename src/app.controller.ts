c15a9994e4a2aa4d54a6fb62383b89fa312e0b1f
nainglinnphyo <nainglinnphyo24@gmail.com>
2025-01-24 14:59:20 +0630

    fix(app): remove test dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,11 @@
-import { AppService } from '@app/app.service';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ExceptionConstants } from '@core/exceptions/constants';
-import { PrismaService } from '@shared/prisma/prisma.service';
 import { BadRequestException } from './core/exceptions/bad-request.exception';
-import { AppDto } from './app.dto';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly dbService: PrismaService,
-  ) {}
-
   @Get()
-  async testException(@Query() dto: AppDto) {
-    console.log(dto);
+  async testException() {
     throw new BadRequestException({
       message: 'Not Allowed',
       cause: new Error('Test exception'),
